56ed69a3bfb50cd2091e82bc7a7e28e415b51203
Kirill Kulakov <kirill578@gmail.com>
2021-04-02 16:30:49 -0500

    Updates

---
+++ precache-manifest.dc8e3e14a354cf6aeafce454839ab75d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7520f0ecd698290cd2bac4e51b3315c8",
+    "url": "/alarm-thermometer/index.html"
+  },
+  {
+    "revision": "83e7aad928ff6e33e29c",
+    "url": "/alarm-thermometer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "20fa579e34da8945f2c3",
+    "url": "/alarm-thermometer/static/js/2.0c463db8.chunk.js"
+  },
+  {
+    "revision": "83e7aad928ff6e33e29c",
+    "url": "/alarm-thermometer/static/js/main.e53449af.chunk.js"
+  },
+  {
+    "revision": "fc65bd20be3f9e9b47a9",
+    "url": "/alarm-thermometer/static/js/runtime-main.98329731.js"
+  }
+]);
